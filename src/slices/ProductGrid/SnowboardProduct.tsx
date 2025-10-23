f934d4c464173db75b3c1131c725fb56a1ded5c6
Zhengdong-uga <zhengdon@umich.edu>
2025-10-23 11:44:03 -0400

    add snowboards section heading, adjust positioning, apply hover ease-in-out effect, apply vertical and horizontal line svgs as well as apply the color changing hover effect

--- src/slices/ProductGrid/SnowboardProduct.tsx
+++ src/slices/ProductGrid/SnowboardProduct.tsx
@@ -1,13 +1,22 @@
 import { ButtonLink } from "@/app/components/ButtonLink";
+import { Heading } from "@/app/components/Heading";
+import { HorizontalLine, VerticalLine } from "@/app/components/Line";
 import { createClient } from "@/prismicio";
 import { Content, isFilled } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
+import clsx from "clsx";
 import React from "react";
 import { FaStar } from "react-icons/fa6";
 
 type Props = {
   id: string;
 };
+
+const VERTICAL_LINE_CLASSES =
+  "absolute top-0 h-full stroke-2 text-stone-300 transition-colors group-hover:text-stone-400";
+
+const HORIZONTAL_LINE_CLASSES =
+  "-mx-8 stroke-2 text-stone-300 transition-colors group-hover:text-stone-400";
 
 export async function SnowboardProduct({ id }: Props) {
   const client = createClient();
@@ -18,24 +27,39 @@ export async function SnowboardProduct({ id }: Props) {
     : "Price Not Available";
 
   return (
-    <div className="group relative mx-auto w-full maxw72 px-8 pt-4">
-      <div className="flex items-center justify-between ~text-sm/2xl">
-        <span>{price}</span>
-        <span className="inline-flex items-center gap-1">
-          <FaStar className="text-yellow-400" /> 37
-        </span>
-      </div>
+    <div>
+      <Heading className="~text-lg/2xl mb-8">Snowboards</Heading>
+      <div className="group relative mx-auto w-full maxw72 px-8 pt-4">
+        <VerticalLine className={clsx(VERTICAL_LINE_CLASSES, "left-4")} />
+        <VerticalLine className={clsx(VERTICAL_LINE_CLASSES, "right-4")} />
+        <HorizontalLine className={HORIZONTAL_LINE_CLASSES} />
 
-      <div className="-mb-1 overflow-hidden py-4">
-        <PrismicNextImage alt="" field={product.data.image} width={250} />
-      </div>
+        <div className="flex items-center justify-between ~text-sm/2xl">
+          <span>{price}</span>
+          <span className="inline-flex items-center gap-1">
+            <FaStar className="text-yellow-400" /> 37
+          </span>
+        </div>
 
-      <h3 className="my-2 text-center font-sans leading-tight ~text-lg/xl">
-        {product.data.name}
-      </h3>
+        <div className="-mb-1 overflow-hidden py-4">
+          <PrismicNextImage
+            alt=""
+            field={product.data.image}
+            width={150}
+            className="mx-auto w-[58%] origin-top transform-gpu transition-transform duration-500 ease-in-out group-hover:scale-150"
+          />
+        </div>
 
-      <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-200 group-hover:opacity-100">
-        <ButtonLink field={product.data.customizer_link}>Customize</ButtonLink>
+        <HorizontalLine className={HORIZONTAL_LINE_CLASSES} />
+        <h3 className="my-2 text-center font-sans leading-tight ~text-lg/xl">
+          {product.data.name}
+        </h3>
+
+        <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-200 group-hover:opacity-100">
+          <ButtonLink field={product.data.customizer_link}>
+            Customize
+          </ButtonLink>
+        </div>
       </div>
     </div>
   );
