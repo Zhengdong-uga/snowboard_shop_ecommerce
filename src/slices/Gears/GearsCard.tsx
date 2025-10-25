b4c8b2228a06fb6ca5b0fd498aa894f17f2a1dbe
Zhengdong-uga <zhengdon@umich.edu>
2025-10-25 17:03:18 -0400

    adjust the props size

--- src/slices/Gears/GearsCard.tsx
+++ src/slices/Gears/GearsCard.tsx
@@ -10,22 +10,24 @@ type Props = {
 // 👇 Keep export name different from the slice component
 export function Gears({ gears, index }: Props) {
   return (
-    <div className="group relative flex flex-col items-center gap-4">
+    <div className="skater group relative flex flex-col items-center gap-4">
       <div className="stack-layout overflow-hidden">
         <PrismicNextImage
           field={gears.data.photo_background}
           width={500}
-          // height={800}
-          imgixParams={{ q: 20 }}
           alt=""
           className="scale-110 transform transition-all duration-1000 ease-in-out group-hover:scale-100 group-hover:brightness-75 group-hover:saturate-[.8]"
         />
         <PrismicNextImage
           field={gears.data.photo_foreground}
-          width={500}
+          width={300}
           alt=""
+          className="transform transition-transform duration-1000 ease-in-out group-hover:scale-110"
         />
-        {gears.data.gear_name}
+        <div className="relative h-48 w-full place-self-end bg-gradient-to-t from-black via-transparent to-transparent"></div>
+        <h3 className="relative grid place-self-end justify-self-start p-2 font-sans text-brand-gray ~text-2xl/3xl">
+          {gears.data.gear_name}
+        </h3>
       </div>
     </div>
   );
