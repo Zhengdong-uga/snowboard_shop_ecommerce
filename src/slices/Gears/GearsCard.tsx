416a449e47a01ca1c365ee0cf5dc9c1fb76408dd
Zhengdong-uga <zhengdon@umich.edu>
2025-10-25 18:09:21 -0400

    add scribble animation for gears

--- src/slices/Gears/GearsCard.tsx
+++ src/slices/Gears/GearsCard.tsx
@@ -1,6 +1,9 @@
+import { ButtonLink } from "@/app/components/ButtonLink";
 import { Content } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
+import clsx from "clsx";
 import React from "react";
+import { GearScribble } from "./GearScribble";
 
 type Props = {
   gears: Content.GearsDocument;
@@ -9,6 +12,16 @@ type Props = {
 
 // 👇 Keep export name different from the slice component
 export function Gears({ gears, index }: Props) {
+  const colors = [
+    "text-brand-blue",
+    "text-brand-lime",
+    "text-brand-orange",
+    "text-brand-pink",
+    "text-brand-purple",
+  ];
+
+  const scribbleColor = colors[index];
+
   return (
     <div className="skater group relative flex flex-col items-center gap-4">
       <div className="stack-layout overflow-hidden">
@@ -18,6 +31,7 @@ export function Gears({ gears, index }: Props) {
           alt=""
           className="scale-110 transform transition-all duration-1000 ease-in-out group-hover:scale-100 group-hover:brightness-75 group-hover:saturate-[.8]"
         />
+        <GearScribble className={clsx("relative", scribbleColor)} />
         <PrismicNextImage
           field={gears.data.photo_foreground}
           width={500}
@@ -30,6 +44,9 @@ export function Gears({ gears, index }: Props) {
           <span className="block">{gears.data.gear_brand}</span>
         </h3>
       </div>
+      <ButtonLink field={gears.data.customizer_link} size="sm">
+        Build your gears
+      </ButtonLink>
     </div>
   );
 }
