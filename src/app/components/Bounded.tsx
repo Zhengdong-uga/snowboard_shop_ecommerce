da79ba39847f737493a7df50ba494d972d1cf0d5
Zhengdong-uga <zhengdon@umich.edu>
2025-11-13 13:36:37 -0500

    fix: disable eslint rule for polymorphic component

--- src/app/components/Bounded.tsx
+++ src/app/components/Bounded.tsx
@@ -14,6 +14,7 @@ export function Bounded({
   children,
   ...restProps
 }: BoundedProps) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const Comp: any = as || "section";
 
   return (
